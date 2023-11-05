fa969044f72202242edda3a1e2a9c85879a25d57
Nacho López Sáez <32782455+nachols1986@users.noreply.github.com>
2023-11-05 11:16:17 -0300

    Add files via upload

---
+++ 06_usuarios_permisos.sql
@@ -0,0 +1,20 @@
+-- Usuario solo_lectura con permisos de solo lectura
+CREATE USER 'solo_lectura'@'localhost' IDENTIFIED BY 'contrasena_solo_lectura';
+
+-- Conceder permisos de solo lectura en todas las tablas del marketplace_db
+-- No se permite eliminar registros
+GRANT SELECT ON marketplace_db.* TO 'solo_lectura'@'localhost';
+
+-- Usuario lectura_insercion_modificacion con permisos de lectura, inserción y modificación
+CREATE USER 'lectura_insercion_modificacion'@'localhost' IDENTIFIED BY
+'contrasena_lectura_insercion_modificacion';
+
+-- Conceder permisos de lectura, inserción y modificación en todas las tablas del marketplace_db
+-- No se permite eliminar registros
+GRANT SELECT, INSERT, UPDATE ON marketplace_db.* TO
+'lectura_insercion_modificacion'@'localhost';
+
+-- Eliminar la capacidad de eliminar registros para ambos usuarios
+-- Esto se aplica a nivel global
+REVOKE DELETE ON *.* FROM 'solo_lectura'@'localhost';
+REVOKE DELETE ON *.* FROM 'lectura_insercion_modificacion'@'localhost'; 
